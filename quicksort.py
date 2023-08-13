2bdfd7e184c9847451bb5dc8a9a8add1c8b88e94
Pedro Luis <pedroluisnr22@gmail.com>
2023-08-13 00:49:10 -0500

    feat: add quick sort

---
+++ quicksort.py
@@ -0,0 +1,27 @@
+# Quick sort
+
+def quicksort(array):
+    size = len(array)
+    _quicksort(array, 0, size - 1)
+
+
+def _quicksort(array, low, high):
+    if low < high:
+        pi = _partition(array, low, high)
+
+        _quicksort(array, low, pi - 1)
+        _quicksort(array, pi + 1, high)
+
+
+def _partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+    return i + 1
