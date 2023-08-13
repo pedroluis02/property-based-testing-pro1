7bd88a89268820d572c223eae01b3fe590071482
Pedro Luis <pedroluisnr22@gmail.com>
2023-08-13 00:49:52 -0500

    feat: generate int values and sort

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import random
+
+from quicksort import quicksort
+
+
+def generate_int_values(length=1, max_value=1000):
+    new_list = []
+    for i in range(0, length):
+        n = random.randint(1, max_value)
+        new_list.append(n)
+    return new_list
+
+
 if __name__ == '__main__':
-    print('Hello World')
+    print('Sort values')
+    values = generate_int_values(10)
+    print(values)
+
+    quicksort(values)
+    print(values)
