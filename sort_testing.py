6f392622171c9ea14f289f2b6696a752d45e2a2c
Pedro Luis <pedroluisnr22@gmail.com>
2023-08-13 23:43:35 -0500

    chrore: add requirements.txt

--- sort_testing.py
+++ sort_testing.py
@@ -9,3 +9,10 @@ def test_list_size(u_list):
     original_len = len(u_list)
     quicksort(u_list)
     assert len(u_list) == original_len
+
+
+@given(strategies.lists(strategies.integers(min_value=1), min_size=1))
+def test_elements_in_order(u_list):
+    quicksort(u_list)
+    for x in range(len(u_list) - 1):
+        assert u_list[x] <= u_list[x + 1]
